4207f9780aa843903b23ccf270dcd51fd2378ee9
hnrt <hnarita@sb3.so-net.ne.jp>
2017-03-20 02:13:03 +0900

    add icons.

--- src/View/PixStore.cc
+++ src/View/PixStore.cc
@@ -1,9 +1,19 @@
 // Copyright (C) 2012-2017 Hideaki Narita
 
 
-#include "PixStore.h"
+#include <gtkmm.h>
+#include "Icon/Hourglass.h"
+#include "Icon/No.h"
+#include "Icon/Pause.h"
+#include "Icon/PowerOff.h"
+#include "Icon/PowerOn.h"
+#include "Icon/Warning.h"
+#include "Icon/Yes.h"
 #include "XenServer/Host.h"
 #include "XenServer/Session.h"
+#include "XenServer/StorageRepository.h"
+#include "XenServer/VirtualMachine.h"
+#include "PixStore.h"
 
 
 using namespace hnrt;
@@ -30,20 +40,62 @@ PixStore& PixStore::instance()
 }
 
 
-PixStore::PixStore()
-    : _pixApp(Gdk::Pixbuf::create_from_file("/usr/share/icons/gnome/32x32/apps/preferences-desktop-remote-desktop.png"))
+static Glib::RefPtr<Gdk::Pixbuf> RenderIcon(const Gtk::StockID& stockId, Gtk::IconSize size)
 {
     Gtk::Invisible w;
-    _pixYes = w.render_icon(Gtk::Stock::YES, Gtk::ICON_SIZE_SMALL_TOOLBAR);
-    _pixNo = w.render_icon(Gtk::Stock::NO, Gtk::ICON_SIZE_SMALL_TOOLBAR);
+    return w.render_icon(stockId, size);
 }
 
 
-Glib::RefPtr<Gdk::Pixbuf> PixStore::get(const RefPtr<Host> host)
+PixStore::PixStore()
+    : _pixApp(Gdk::Pixbuf::create_from_file("/usr/share/icons/gnome/32x32/apps/preferences-desktop-remote-desktop.png"))
+    , _pixError(RenderIcon(Gtk::Stock::DIALOG_ERROR, Gtk::ICON_SIZE_BUTTON))
+    , _pixHourglass(Gdk::Pixbuf::create_from_inline(-1, _iconHourglass, false))
+    , _pixNo(Gdk::Pixbuf::create_from_inline(-1, _iconNo, false))
+    , _pixPause(Gdk::Pixbuf::create_from_inline(-1, _iconPause, false))
+    , _pixPowerOff(Gdk::Pixbuf::create_from_inline(-1, _iconPowerOff, false))
+    , _pixPowerOn(Gdk::Pixbuf::create_from_inline(-1, _iconPowerOn, false))
+    , _pixWarning(Gdk::Pixbuf::create_from_inline(-1, _iconWarning, false))
+    , _pixYes(Gdk::Pixbuf::create_from_inline(-1, _iconYes, false))
+{
+}
+
+
+Glib::RefPtr<Gdk::Pixbuf> PixStore::get(RefPtr<Host> host)
 {
     return
-        !host ? _pixNo :
-        host->isBusy() ? _pixYes :
+        !host ? _pixError :
+        host->isBusy() ? _pixHourglass :
         host->getSession().isConnected() ? _pixYes :
         _pixNo;
+}
+
+
+Glib::RefPtr<Gdk::Pixbuf> PixStore::get(RefPtr<StorageRepository> sr)
+{
+    return _pixError;
+}
+
+
+Glib::RefPtr<Gdk::Pixbuf> PixStore::get(RefPtr<VirtualMachine> vm)
+{
+    if (!vm)
+    {
+        return _pixError;
+    }
+    else if (vm->isBusy())
+    {
+        return _pixHourglass;
+    }
+    else
+    {
+        XenPtr<xen_vm_record> record = vm->getRecord();
+        return
+            //record->is_a_snapshot ? _pixComputer :
+            record->power_state == XEN_VM_POWER_STATE_HALTED ? _pixPowerOff :
+            record->power_state == XEN_VM_POWER_STATE_PAUSED ? _pixWarning :
+            record->power_state == XEN_VM_POWER_STATE_RUNNING ? _pixPowerOn :
+            record->power_state == XEN_VM_POWER_STATE_SUSPENDED ? _pixPause :
+            _pixError;
+    }
 }
--- src/View/PixStore.h
+++ src/View/PixStore.h
@@ -12,6 +12,8 @@
 namespace hnrt
 {
     class Host;
+    class StorageRepository;
+    class VirtualMachine;
 
     class PixStore
     {
@@ -25,7 +27,9 @@ namespace hnrt
         Glib::RefPtr<Gdk::Pixbuf> getApp() const { return _pixApp; }
         Glib::RefPtr<Gdk::Pixbuf> getYes() const { return _pixYes; }
         Glib::RefPtr<Gdk::Pixbuf> getNo() const { return _pixNo; }
-        Glib::RefPtr<Gdk::Pixbuf> get(const RefPtr<Host>);
+        Glib::RefPtr<Gdk::Pixbuf> get(RefPtr<Host>);
+        Glib::RefPtr<Gdk::Pixbuf> get(RefPtr<StorageRepository>);
+        Glib::RefPtr<Gdk::Pixbuf> get(RefPtr<VirtualMachine>);
 
     protected:
 
@@ -34,8 +38,14 @@ namespace hnrt
         void operator =(const PixStore&);
 
         Glib::RefPtr<Gdk::Pixbuf> _pixApp;
-        Glib::RefPtr<Gdk::Pixbuf> _pixYes;
+        Glib::RefPtr<Gdk::Pixbuf> _pixError;
+        Glib::RefPtr<Gdk::Pixbuf> _pixHourglass;
         Glib::RefPtr<Gdk::Pixbuf> _pixNo;
+        Glib::RefPtr<Gdk::Pixbuf> _pixPause;
+        Glib::RefPtr<Gdk::Pixbuf> _pixPowerOff;
+        Glib::RefPtr<Gdk::Pixbuf> _pixPowerOn;
+        Glib::RefPtr<Gdk::Pixbuf> _pixWarning;
+        Glib::RefPtr<Gdk::Pixbuf> _pixYes;
     };
 }
 
